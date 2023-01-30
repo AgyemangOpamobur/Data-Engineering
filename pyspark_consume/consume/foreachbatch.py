7b77cb28f797785e0035f1ff0468504c2c5b2fd7
Agyemang Alex <78166528+AgyemangOpamobur@users.noreply.github.com>
2023-01-30 14:38:28 +0000

    Add files via upload

--- pyspark_consume/consume/foreachbatch.py
+++ pyspark_consume/consume/foreachbatch.py
@@ -5,6 +5,6 @@ def foreach_batch_function(df, epoch_id):
     df.select("*").write.format("jdbc")\
         .mode("append")\
         .option("url", f"jdbc:postgresql://{settings.database_hostname}:{settings.database_port}/{settings.database_name}") \
-        .option("driver", "org.postgresql.Driver").option("dbtable", "Environment_Readings") \
+        .option("driver", "org.postgresql.Driver").option("dbtable", "environment_readings") \
         .option("user", settings.database_username).option("password", settings.database_password).save()
     pass
